c4780dc0c3076c6bec49a1b55fc174c7384400cc
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2022-06-26 21:24:38 +0400

    v2.0

--- Js/index.script.js
+++ Js/index.script.js
@@ -1,11 +1,60 @@
+let mouseCursor = document.querySelector('.cursor')
+
+window.addEventListener("mousemove", cursor);
+
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
+}
+
+const navLinksA = document.querySelectorAll('.nav-links li a');
+
+navLinksA.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow");
+    });
+    link.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow");
+    });
+})
+
+const logo = document.querySelector('.logo a')
+
+logo.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+});
+logo.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+});
+
+const h1Txt = document.querySelector('.banner .Give-up')
+
+h1Txt.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("txt-grow");
+});
+h1Txt.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("txt-grow");
+});
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav  = document.querySelector('.nav-links');
+    const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    const mobNav = document.querySelector('.mobile-menu-bg');
+
+    burger.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("burger-grow");
+        burger.classList.add("hovered-link");
+    });
+    burger.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("burger-grow");
+        burger.classList.remove("hovered-link");
+    });
 
     burger.addEventListener('click', () => {
         //Toggle Nav
-        nav.classList.toggle('nav-active');
+        // nav.classList.toggle('nav-active');
+        mobNav.classList.toggle('nav-active');
 
         //Animate Links
         navLinks.forEach((link, index) => {
@@ -26,13 +75,14 @@ const navSlide = () => {
 navSlide();
 
 //Typewriter animation
-var i=0;
+var i = 0;
 var txt = "NEVER";
 var speed = 750;
+
 function type() {
     document.querySelector('.never').innerHTML += txt.charAt(i++);
-    i+0.1;
-    setTimeout(type,speed);
+    i + 0.1;
+    setTimeout(type, speed);
 }
 
 type();
